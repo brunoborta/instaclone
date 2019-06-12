3d03ec0399e9c443cb0f85ed03accaca54cf5afc
Bruno "Borta" Bortagaray <bruno.bortagaray@gmail.com>
2019-06-12 11:03:38 -0300

    Added backend

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect('mongodb+srv://instaclone:instaclone@cluster0-tgzep.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+})
+
+// Todas as urls podem acessar o app
+app.use(cors());
+
+//cria uma rota files onde acessa /uploads/resized/nomeDaImagem.jpeg
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
+
+server.listen(3000, () => {
+  console.log("Rodando na porta 3000");
+});
